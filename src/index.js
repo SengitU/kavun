9178dd5eed69976a372bd1eef3f78d31633eea3e
Ugurcan Sengit <ugurcansengit@gmail.com>
2018-04-03 16:10:39 +0200

    Added spec definition. There should be a better way.

--- src/index.js
+++ src/index.js
@@ -1,36 +1,59 @@
 const assert = require('assert');
 
-const spec = () => {};
+const beaver = (() => {
+  const tests = {};
 
-const unit = (test) => {
-  try {
-    test.apply();
-    return true;
-  } catch(error) {
-    return false;
+  let currentSpecId = 0;
+
+  const execute = (test) => {
+    try {
+      test();
+      return true;
+    } catch(error) {
+      return false;
+    }
+  };
+
+  // No more standalone, executable units anymore 🤦‍
+  const unit = test => tests[currentSpecId].push(test);
+  
+  const spec = (specCallback) => {
+    tests[currentSpecId] = [];
+
+    specCallback();
+
+    const testResults = tests[currentSpecId].map(test => execute(test));
+
+    currentSpecId += 1;
+
+    if(testResults.indexOf(false) > -1) {
+      return 'Some tests are failed';
+    } else {
+      return 'All tests passed';
+    }
+  };
+
+  return {
+    spec,
+    unit
   }
-};
+})();
 
-assert.equal(
-  true,
-  // Should return true
-  unit(() => {
-    assert.equal(1, 1)
-  })
-);
 
-assert.equal(
-  false,
-  // Should return false
-  unit(() => {
-    assert.equal(0, 1)
-  })
-);
+const { unit, spec } = beaver;
 
 assert.equal(
   'All tests passed',
   spec(() => {
     unit(() => assert.equal(1, 1));
+    unit(() => assert.equal(2, 2));
+  })
+);
+
+assert.equal(
+  'Some tests are failed',
+  spec(() => {
+    unit(() => assert.equal(0, 1));
     unit(() => assert.equal(2, 2));
   })
 );
