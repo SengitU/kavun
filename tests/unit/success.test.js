10e1939517b9b5ac3fc67fed9014be35722459fd
Ugurcan Sengit <ugurcansengit@gmail.com>
2018-04-04 15:34:17 +0200

    Refactored lib to be able to collect descriptions

--- tests/unit/success.test.js
+++ tests/unit/success.test.js
@@ -5,6 +5,9 @@ const assert = require('assert');
 const { unit } = beaver;
 logMock.apply();
 
-unit(() => assert.equal(1, 1));
+unit('1 should equal 1',() => assert.equal(1, 1));
 
-setTimeout(() => assert.equal(true, logMock.getMessages().indexOf('All tests passed') > -1), 0);
+setTimeout(() => {
+  assert.equal(true, logMock.getMessages().indexOf('1 should equal 1 => ✓') > -1);
+  assert.equal(true, logMock.getMessages().indexOf('All tests passed') > -1);
+}, 0);
