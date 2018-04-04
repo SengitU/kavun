3867d4a6a093bb5ec0cd7f34f77e90d0382acbaf
Ugurcan Sengit <ugurcansengit@gmail.com>
2018-04-04 15:39:03 +0200

    Updated failed test case for unit

--- tests/unit/failure.test.js
+++ tests/unit/failure.test.js
@@ -5,6 +5,9 @@ const assert = require('assert');
 const { unit } = beaver;
 logMock.apply();
 
-unit(() => assert.equal(0, 1));
+unit('0 should equal 0', () => assert.equal(0, 1));
 
-setTimeout(() => assert.equal(true, logMock.getMessages().indexOf('1 failure, 0 pass') > -1), 0);
+setTimeout(() => {
+  assert.equal(true, logMock.getMessages().indexOf('0 should equal 0 => X') > -1);
+  assert.equal(true, logMock.getMessages().indexOf('1 failure, 0 pass') > -1);
+}, 0);
