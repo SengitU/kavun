97b1a7def3ed0c1a36c0fda10685d77e54360d49
Ugurcan Sengit <ugurcansengit@gmail.com>
2018-04-04 13:22:02 +0200

    Simplified definition of spec and unit

--- tests/spec/no-failure.test.js
+++ tests/spec/no-failure.test.js
@@ -2,18 +2,18 @@ const beaver = require('../../lib');
 const logMock = require('../../utils/log-mock');
 const assert = require('assert');
 
-const { spec } = beaver;
+const { spec, unit } = beaver;
 
 logMock.apply();
 
-spec((unit) => {
+spec(() => {
   unit(() => assert.equal(1, 1));
   unit(() => assert.equal(2, 2));
 });
 
-spec((unit) => {
+spec(() => {
   unit(() => assert.equal(3, 3));
   unit(() => assert.equal(4, 4));
 });
 
-setTimeout(() => assert.equal(true, logMock.getMessages().indexOf('All tests passed') > -1), 1000);
+setTimeout(() => assert.equal(true, logMock.getMessages().indexOf('All tests passed') > -1), 0);
