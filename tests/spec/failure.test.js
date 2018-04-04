234afe5d0d06a76ba5c12c4943ebbaf8cf2497fc
Ugurcan Sengit <ugurcansengit@gmail.com>
2018-04-04 13:35:56 +0200

    Added reporter

--- tests/spec/failure.test.js
+++ tests/spec/failure.test.js
@@ -8,6 +8,12 @@ logMock.apply();
 spec(() => {
   unit(() => assert.equal(0, 1));
   unit(() => assert.equal(2, 2));
+  unit(() => assert.equal(1, 1));
+  unit(() => assert.equal(0, 2));
+  unit(() => assert.equal(1, 1));
+  unit(() => assert.equal(2, 2));
+  unit(() => assert.equal(0, 1));
+  unit(() => assert.equal(2, 2));
 });
 
-setTimeout(() => assert.equal(true, logMock.getMessages().indexOf('Some tests failed') > -1), 0);
+setTimeout(() => assert.equal(true, logMock.getMessages().indexOf('3 failure, 5 pass') > -1), 0);
--- tests/unit/failure.test.js
+++ tests/unit/failure.test.js
@@ -7,4 +7,4 @@ logMock.apply();
 
 unit(() => assert.equal(0, 1));
 
-setTimeout(() => assert.equal(true, logMock.getMessages().indexOf('Some tests failed') > -1), 0);
+setTimeout(() => assert.equal(true, logMock.getMessages().indexOf('1 failure, 0 pass') > -1), 0);
