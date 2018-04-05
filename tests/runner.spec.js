0352813201df6975fc87c1961c9b0d0cbbd2e151
Ugurcan Sengit <ugurcansengit@gmail.com>
2018-04-05 14:20:25 +0200

    Added runner

--- tests/runner.spec.js
+++ tests/runner.spec.js
@@ -1,7 +1,50 @@
+const sinon = require('sinon');
 const assert = require('assert');
 const runner = require('../lib/runner');
-const logMock = require('../utils/log-mock');
 
-logMock.apply();
+const reporter = {
+  step: sinon.spy(),
+  result: sinon.spy()
+};
 
+(() => {
+  // spec('runner', (unit) => {
+  //   unit('should execute single executable and report results for steps and overall to reporter', () => {
+      const description = 'test';
+      const executable = () => {};
+      const executables = [{type: 'unit', description, executable }];
+      const execute = sinon.stub().returns(true);
 
+      runner(executables, { reporter, execute });
+
+      assert(execute.called);
+      assert(reporter.step.calledWith(description, true));
+      assert(reporter.result.calledWith(1, 0));
+  //   });
+  // });
+})();
+
+reporter.step.resetHistory();
+reporter.result.resetHistory();
+
+(() => {
+  // spec('runner', (unit) => {
+  //   unit('should execute a spec of executables and report results for steps and overall to reporter', () => {
+  const specDescription = 'spec';
+  const unitDescription = 'unit';
+  const executable = () => {};
+  const executables = [{type: 'spec', specDescription, tests: [
+    {type: 'unit', description: unitDescription, executable },
+    {type: 'unit', description: unitDescription, executable }
+  ]}];
+  const execute = sinon.stub().returns(true);
+
+  runner(executables, { reporter, execute });
+
+  assert(execute.called);
+  assert(reporter.step.calledWith(specDescription));
+  assert(reporter.step.calledWith(unitDescription, true));
+  assert(reporter.result.calledWith(2, 0));
+  //   });
+  // });
+})();
