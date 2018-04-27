f369d670bd226470132952c51d91906afa7f9824
Ugurcan Sengit <ugurcansengit@gmail.com>
2018-04-27 18:02:38 +0200

    Handle errors other than AssertionError too
    Library was not able to handle any kind of error except AssertionError.
    This should not be the case for a spec runner, it should be able to
    provide feedback against any kind of error

--- tests/execute.spec.js
+++ tests/execute.spec.js
@@ -5,24 +5,39 @@ const { spec, unit } = require('../lib');
 spec('execute', () => {
   unit('should return true for succeeding executable', async () => {
     const succeedingExecutable = () => assert.equal(1, 1);
-    await execute(succeedingExecutable).then(res => assert(res));
+    await execute(succeedingExecutable).then(res => assert(res.result));
   });
 
-  unit('should return expected and actual values for failing executable', async () => {
-    const failingExecutable = () => assert.equal(0, 1);
-    const failureObj = {
-      expected: 1,
-      actual: 0
-    };
-    await execute(failingExecutable).then(res => assert.deepEqual(failureObj, res));
+  spec('Assertion Error', () => {
+    unit('should return expected and actual values for failing executable', async () => {
+      const failingExecutable = () => assert.equal(0, 1);
+      const failureObj = {
+        result: false,
+        description: `AssertionError: 0 == 1`
+      };
+      await execute(failingExecutable).then(res => assert.deepEqual(failureObj, res));
+    });
+  
+    unit('should be able to execute async functions', async () => {
+      const failingExecutable = () => new Promise((resolve) => resolve(assert.equal(0, 1)));
+      const failureObj = {
+        result: false,
+        description: `AssertionError: 0 == 1`
+      };
+      await execute(failingExecutable).then(res => assert.deepEqual(failureObj, res));
+    });
   });
 
-  unit('should be able to execute async functions', async () => {
-    const failingExecutable = () => new Promise((resolve) => resolve(assert.equal(0, 1)));
-    const failureObj = {
-      expected: 1,
-      actual: 0
-    };
-    await execute(failingExecutable).then(res => assert.deepEqual(failureObj, res));
+  spec('Other Errors', () => {
+    unit('should return description and stack trace', async () => {
+      const referrenceErrorExecutable = () => assert(notDefined);
+      const failureObj = {
+        result: false,
+        reason: 'ReferenceError: notDefined is not defined'
+      }
+      const actualFailureObj = await execute(referrenceErrorExecutable);
+      
+      assert.equal(failureObj.result, actualFailureObj.result);
+    });
   });
 });
