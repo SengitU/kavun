a73c9fb2ada7eddeb3b8391f19a11b55c81df219
Ugurcan Sengit <ugurcansengit@gmail.com>
2018-05-21 14:05:12 +0200

    Add stopTimer

--- tests/time-tracker.spec.js
+++ tests/time-tracker.spec.js
@@ -17,9 +17,16 @@ const startTimer = (id) => {
   }
 }
 
+const stopTimer = (id) => {
+  memory[id] = {
+    stop: process.hrtime()
+  }
+}
+
 spec('TimeTracker', () => {
   spec('startTimer', () => {
     unit('should add provided key cache with start values', () => {
+    unit('should add provided key to the cache with start values', () => {
       const id = "random-id";
       
       startTimer(id);
@@ -28,4 +35,14 @@ spec('TimeTracker', () => {
       assert.equal(memory[id].start.length, 2);
     });
   });
+
+  spec("stopTimer", () => {
+    unit("should add end values to the provided key in the cache", () => {
+      const id = "random-id";
+
+      stopTimer(id);
+
+      assert.notEqual(memory[id].stop, undefined);
+      assert.equal(memory[id].stop.length, 2);
+    });
 });
