0eac33049f51de0fc82d5bc39a8c4551757c7afc
Wolfram Kriesing <wolfram+github@kriesing.de>
2018-11-05 09:14:05 +0100

    Implement the new timer, that manages it's state itself.

--- tests/time-tracker.spec.js
+++ tests/time-tracker.spec.js
@@ -1,34 +1,8 @@
 import assert from 'assert';
 import { describe, it } from '../lib';
-import { startTimer, stopTimer } from '../lib/utils/time-tracker';
-
-const memory = {};
+import { startTimer, stopTimer, newStartTimer } from '../lib/utils/time-tracker';
 
 describe('TimeTracker', () => {
-  describe('startTimer', () => {
-    it('should add provided key to the cache with start values', () => {
-      const id = 'random-id';
-      
-      startTimer(id, memory);
-
-      assert.notEqual(memory[id], undefined);
-      assert.equal(memory[id].length, 2);
-    });
-  });
-
-  describe('stopTimer', () => {
-    it('should return elapsed time in miliseconds', () => {
-      const id = 'random-id';
-
-      const elapsedTime = stopTimer(id, memory);
-
-      assert(elapsedTime > 0)
-    });
-  });
-  
-  const newStartTimer = () => {
-    return () => {};
-  };
   it('`startTimer()` returns a function (`stopTimer`)', () => {
     const stopTimer = newStartTimer();
     assert.equal(typeof stopTimer, 'function');
@@ -37,5 +11,12 @@ describe('TimeTracker', () => {
     const stopTimer1 = newStartTimer();
     const stopTimer2 = newStartTimer();
     assert.notEqual(stopTimer1, stopTimer2);
+  });
+  describe('newStopTimer', () => {
+    it('should return elapsed time in miliseconds', () => {
+      const stopTimer = newStartTimer();
+      const elapsedTime = stopTimer();
+      assert(elapsedTime > 0)
+    });
   });
 });
