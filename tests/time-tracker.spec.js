e0afb33b6d5aa64270528b3228f9972d344bd33c
Wolfram Kriesing <wolfram+github@kriesing.de>
2018-11-05 09:31:28 +0100

    Rename `newStartTimer` to `startTimer`.

--- tests/time-tracker.spec.js
+++ tests/time-tracker.spec.js
@@ -1,20 +1,20 @@
 import assert from 'assert';
 import { describe, it } from '../lib';
-import { newStartTimer } from '../lib/utils/time-tracker';
+import { startTimer } from '../lib/utils/time-tracker';
 
 describe('TimeTracker', () => {
   it('`startTimer()` returns a function (`stopTimer`)', () => {
-    const stopTimer = newStartTimer();
+    const stopTimer = startTimer();
     assert.equal(typeof stopTimer, 'function');
   });
   it('calling `startTimer()` twice returns a different function every time', () => {
-    const stopTimer1 = newStartTimer();
-    const stopTimer2 = newStartTimer();
+    const stopTimer1 = startTimer();
+    const stopTimer2 = startTimer();
     assert.notEqual(stopTimer1, stopTimer2);
   });
   describe('newStopTimer', () => {
     it('should return elapsed time in miliseconds', () => {
-      const stopTimer = newStartTimer();
+      const stopTimer = startTimer();
       const elapsedTime = stopTimer();
       assert(elapsedTime > 0)
     });
