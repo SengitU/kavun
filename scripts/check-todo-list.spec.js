9adc5da65913f66a5415d01aade84ead541a164c
Wolfram Kriesing <wolfram+github@kriesing.de>
2018-11-26 21:52:14 +0100

    Simplest impl.

--- scripts/check-todo-list.spec.js
+++ scripts/check-todo-list.spec.js
@@ -1,9 +1,13 @@
 import { it, describe } from '../lib';
 import assert from 'assert';
 
+const parseChangelog = () => {
+  return { version: -1, items: [] };
+};
+
 describe('Parse a CHANGELOG.md', () => {
   it('WHEN empty return no info', () => {
     const empty = '';
-    assert.equal(parseChangelog(empty), { version: -1, items: [] });
+    assert.deepEqual(parseChangelog(empty), { version: -1, items: [] });
   });
 });
